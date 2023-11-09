106e9fc940026b48ecf0acf49728b1fd70bc3f61
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-09 22:05:46 +0530

    gives pretty good resukts; recienved reuslt in about 45 secs

--- Telegram/answer.py
+++ Telegram/answer.py
@@ -1,8 +1,8 @@
 import os
 import json
 from dotenv import load_dotenv
-from langchain import PromptTemplate
-from langchain.agents import initialize_agent,Tool
+from langchain.prompts import PromptTemplate
+from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.prompts import MessagesPlaceholder
 from langchain.chains.summarize import load_summarize_chain
@@ -25,36 +25,23 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 def search(query):
     url = "https://google.serper.dev/search"
 
-    payload = json.dumps({
-        "q": query
-    })
+    payload = json.dumps({"q": query})
 
-    headers = {
-
-        'X-API-KEY': serper_api_key,
-        'Content-Type': 'application/json'
-    }
+    headers = {"X-API-KEY": serper_api_key, "Content-Type": "application/json"}
 
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.text
 
 
-def scrape_website(objective:str , url:str):
-    headers= {
-        'Cache-Control' : 'no-cache',
-        'Content-Type': 'application.json'
-    }
-    data= {
-        "url":url
-    }
+def scrape_website(objective: str, url: str):
+    headers = {"Cache-Control": "no-cache", "Content-Type": "application.json"}
+    data = {"url": url}
 
     data_json = json.dumps(data)
 
-
     post_url = f"https://chrome.browserless.io/content?token={browserless_api_key}"
 
-    response = requests.post (post_url , headers= headers , data=data_json)
-
+    response = requests.post(post_url, headers=headers, data=data_json)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
@@ -69,33 +56,42 @@ def scrape_website(objective:str , url:str):
         print(f"HTTP request failed with status code {response.status_code}")
 
 
-def summary(objective,content):
-    llm = ChatOpenAI(temperature=0.4, model= "gpt-3.5-turbo-16k-0613", openai_api_key=openai_api_key)
-    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size = 4000 , chunk_overlap = 200)
+def summary(objective, content):
+    llm = ChatOpenAI(
+        temperature=0.4, model="gpt-3.5-turbo-16k-0613", openai_api_key=openai_api_key
+    )
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=4000, chunk_overlap=200
+    )
     docs = text_splitter.create_documents([content])
-    map_prompt ="""
+    map_prompt = """
 Write a summary of the following text for {objective}:
 "{text}"
 SUMMARY:
 """
 
-    map_prompt_template = PromptTemplate (template = map_prompt, input_variables= ["text","objective"])
+    map_prompt_template = PromptTemplate(
+        template=map_prompt, input_variables=["text", "objective"]
+    )
     summary_chain = load_summarize_chain(
         llm=llm,
-        chain_type='map_reduce',
+        chain_type="map_reduce",
         map_prompt=map_prompt_template,
         combine_prompt=map_prompt_template,
-        verbose=True
+        verbose=True,
     )
 
     output = summary_chain.run(input_documents=docs, objective=objective)
 
     return output
 
+
 class ScrapeWebsiteInput(BaseModel):
     """Inputs for scrape_website"""
+
     objective: str = Field(
-        description="The objective & task that users give to the agent")
+        description="The objective & task that users give to the agent"
+    )
     url: str = Field(description="The url of the website to be scraped")
 
 
@@ -110,11 +106,12 @@ class ScrapeWebsiteTool(BaseTool):
     def _arun(self, url: str):
         raise NotImplementedError("error here")
 
+
 tools = [
     Tool(
         name="Search",
         func=search,
-        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+        description="useful for when you need to answer questions about current events, data. You should ask targeted questions",
     ),
     ScrapeWebsiteTool(),
 ]
@@ -134,7 +131,8 @@ For each research task, You will:
 4) Your goal is to provide the most accurate, fact-based research results through a thorough process.
 5 In the final output, You will provide all credible references and sources to back up my research findings.
 6) In the final output, You will provide all credible references and sources to back up my research findings.
-""")
+"""
+)
 
 agent_kwargs = {
     "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
@@ -143,8 +141,8 @@ agent_kwargs = {
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 memory = ConversationSummaryBufferMemory(
-    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
-
+    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000
+)
 
 
 agent = initialize_agent(
@@ -155,3 +153,14 @@ agent = initialize_agent(
     agent_kwargs=agent_kwargs,
     memory=memory,
 )
+
+def main():
+    query = "Explain graphql to me in simple words"
+    result = agent ({"input": query})
+    return result 
+
+
+result = main()
+print(result)
+if __name__ == '__main__':
+    main()
