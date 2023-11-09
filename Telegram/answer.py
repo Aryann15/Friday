9c5da9e04abf7c83130cfbcbe9b6dc162a578194
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-09 20:27:33 +0530

    able tp search web

--- Telegram/answer.py
+++ Telegram/answer.py
@@ -1,10 +1,30 @@
 import os
+import json
 from dotenv import load_dotenv
-
+from langchain import PromptTemplate
+import requests
 
 load_dotenv()
 browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-query = "Explain TRPC to me in 30 minutes"
+
+def search(query):
+    url = "https://google.serper.dev/search"
+
+    payload = json.dumps({
+        "q": query
+    })
+
+    headers = {
+
+        'X-API-KEY': serper_api_key,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.text
+
+result = search("Hashnode")
+print(result)
