3a938f1a6c01b53133a7b6330eb0714783e7097d
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-10 11:26:18 +0530

    greetings added to bot

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -1,6 +1,16 @@
 from dotenv import load_dotenv
 import os 
+import telebot
+
 
 load_dotenv()
 tg_bot_key = os.getenv("BOT_API")
-print (tg_bot_key)
+bot = telebot.TeleBot (tg_bot_key)
+
+@bot.message_handler(['start'])
+def start ( message): 
+    bot.reply_to(message, "Hello Welcom aboard chief!")
+    bot.send_message (message.chat.id, "What can i do for Today !")
+
+print("done")
+bot.polling()
