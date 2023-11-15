abff9fdc97608cd29bff9c5bf22401d5f86fd98f
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-16 01:22:17 +0530

    selecting text using ceontext menu

--- linkedin/public/background.js
+++ linkedin/public/background.js
@@ -1,34 +1,13 @@
+console.log("Hello from background script");
 
-// console.log("Background script loaded!");
-
-
-// // chrome.commands.onCommand.addListener((command) => {
-// //   console.log(`Command "${command}" triggered`);
-// // });
-
-// browser.commands.onCommand.addListener((command) => {
-//   if (command === "copy_to_console") {
-//     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-//       chrome.tabs.executeScript(
-//         tabs[0].id, 
-//         {code: 'window.getSelection().toString();'},
-//         (selection) => {
-//           console.log(selection[0]);
-//         }
-//       );    
-//     });    
-//   }
-// });
-
-
+// background.js
 
 chrome.contextMenus.create({
   id: "friday",
   title: "Ask Friday",
-  contexts : ['selection', 'link']
+  contexts: ["selection", "link"],
+});
 
-})
-
-chrome.contextMenus.onClicked.addListener((info,tab)=>{
-   console.log(info.selectionText.trim())
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const selectedText = info.selectionText.trim();
 })
