f30f88dc9207f90673edd92b9a667b3bad6a5feb
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-16 01:24:04 +0530

    receivinf text from background.js

---
+++ linkedin/public/popup.js
@@ -0,0 +1,9 @@
+console.log('Popup script loaded');
+
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.type === "selected-text") {
+    const selectedText = request.text;
+    console.log(selectedText)
+  }
+});
