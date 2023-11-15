f2b1c3c0bdc8a437795ae20b0f35d4afe61b5896
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-16 02:15:54 +0530

    Handle selected text from popup in App component

--- linkedin/public/popup.js
+++ linkedin/public/popup.js
@@ -1,9 +1,9 @@
 console.log('Popup script loaded');
 
-
 chrome.runtime.onMessage.addListener((request, sender) => {
   if (request.type === "selected-text") {
     const selectedText = request.text;
     console.log(selectedText)
+    window.postMessage({ type: "selected-text", text: selectedText }, "*");
   }
 });
--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App () {
+const App = () => {
+  const [selectedText, setSelectedText] = useState("");
 
-  const handleClick =() => {
-    alert("welcome mr stark!")
-  }
+  useEffect(() => {
+    // Add an event listener to listen for messages from the popup
+    const handleMessage = (event) => {
+      // Check if the message is from the popup and has the expected type
+      if (event.data && event.data.type === "selected-text") {
+        const text = event.data.text;
+
+        // Handle the selected text in your React component
+        setSelectedText(text);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    // Remove the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []); // Empty dependency array ensures the effect runs once during component mount
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="extension-title">Friday Extension</h1>
-        <button onClick={handleClick} className="cta-button">
-          Click me!
-        </button>
+        <h1 className="extension-title">Friday</h1>
+        <button className="cta-button">Click me!</button>
         <div className="input-container">
-          <input type="text" placeholder="Your name" required={true} className="input-field" />
-          <input type="text" placeholder="Your thoughts" className="input-field" />
+          <input
+            type="text"
+            placeholder="Your name"
+            required={true}
+            className="input-field"
+          />
+          <input
+            type="text"
+            placeholder="Your thoughts"
+            className="input-field"
+          />
           <button className="submit-button">Submit</button>
+          <h4> Selected Text: {selectedText}</h4>
         </div>
       </header>
     </div>
   );
-  
-  }
+};
+
 export default App;
