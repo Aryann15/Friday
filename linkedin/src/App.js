f3eaa2d7ac45ba3478ab6767b9f45d70884677e3
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-17 00:34:39 +0530

    set the names and topics

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -3,27 +3,28 @@ import "./App.css";
 
 const App = () => {
   const [selectedText, setSelectedText] = useState("");
-
+  const [topic, setTopic] = useState("");
+  const [name, setName] = useState("");
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleTopicChange = (event) => {
+    setTopic(event.target.value);
+  };
   useEffect(() => {
-    // Add an event listener to listen for messages from the popup
+    // Adding an event listener to listen for messages from the popup
     const handleMessage = (event) => {
-      // Check if the message is from the popup and has the expected type
       if (event.data && event.data.type === "selected-text") {
         const text = event.data.text;
 
-        // Handle the selected text in your React component
         setSelectedText(text);
       }
     };
-
     window.addEventListener("message", handleMessage);
-
-    // Remove the event listener when the component is unmounted
     return () => {
       window.removeEventListener("message", handleMessage);
     };
-  }, []); // Empty dependency array ensures the effect runs once during component mount
-
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -35,11 +36,13 @@ const App = () => {
             placeholder="Your name"
             required={true}
             className="input-field"
+            onChange={handleNameChange}
           />
           <input
             type="text"
             placeholder="Your thoughts"
             className="input-field"
+            onChange={handleTopicChange}
           />
           <button className="submit-button">Submit</button>
           <h4> Selected Text: {selectedText}</h4>
