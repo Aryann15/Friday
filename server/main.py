3c65949fe92868d3b084e9d180314fba66a7cfd4
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-17 02:18:45 +0530

    insertng texts, name nand topic to the prompt template and returning result

--- server/main.py
+++ server/main.py
@@ -26,8 +26,15 @@ def handle_messages():
 
 """
         openai = OpenAI(model_name="gpt-3.5-turbo-1106", openai_api_key=openai_api_key)
+        openai.temperature = 0.8
         prompt_template = PromptTemplate(input_variables=["text","name","topic"], template=template)
+        result = openai(prompt_template.format(
+            text= texts,
+            name= name,
+            topic = topic
+        ))
 
+        return result
 
 if __name__ == "__main__":
     app.run(debug=True)
