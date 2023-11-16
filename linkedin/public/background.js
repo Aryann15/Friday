49f772cc16186665c8be13d99d2f20972dba5fe7
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-17 05:28:15 +0530

    adding job details context menu

--- linkedin/public/background.js
+++ linkedin/public/background.js
@@ -1,14 +1,35 @@
 console.log("Hello from background script");
 
-// background.js
+chrome.contextMenus.create({
+  id: "friday@chat",
+  title: "Ask Friday@chat",
+  contexts: ["selection", "link"],
+})
 
 chrome.contextMenus.create({
-  id: "friday",
-  title: "Ask Friday",
+  id: "friday@job",
+  title: "Ask Friday@job",
   contexts: ["selection", "link"],
-});
+})
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  const selectedText = info.selectionText;
-  chrome.runtime.sendMessage({ type: 'selected-text', text: selectedText });
+chrome.contextMenus.onClicked.addListener(info => {
+  if (info.menuItemId === "friday@chat") {
+    // Handle Friday@chat click
+    const selectedText = info.selectionText;
+    chrome.runtime.sendMessage({ 
+      type: 'chat-text', 
+      text: selectedText,
+      source: "friday@chat" 
+    });
+  }
+
+  if (info.menuItemId === "friday@job") {
+    // Handle Friday@job click
+    const selectedText = info.selectionText;
+    chrome.runtime.sendMessage({ 
+      type: 'job-details-text', 
+      text: selectedText,
+      source: "friday@job" 
+    });  
+  }
 })
