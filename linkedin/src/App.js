84588022a4ae661cf6beaa636e89f6a4c90aff3c
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-18 02:50:31 +0530

    updated app with classnames

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,26 +1,43 @@
+// App.js
+
 import React, { useState } from 'react';
-import './App.css';
 import ChatHelp from './components/ChatHelp';
+import CoverLetterHelper from './components/CoverLetterHelper';
+import './App.css';
+
 function App() {
   const [content, setContent] = useState('');
+  const [selectedSubHeading, setSelectedSubHeading] = useState('');
+
   const handleSubHeadingClick = (text) => {
+    setSelectedSubHeading(text);
     if (text === 'Chat Help') {
       setContent(<ChatHelp />);
     } else if (text === 'Cover Letter') {
-      setContent('These are the job details.');
+      setContent(<CoverLetterHelper />);
     }
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app-container">
+      <header className="app-header">
         <h1>Friday</h1>
-        <div>
-          <h2 onClick={() => handleSubHeadingClick('Chat Help')}>Chat</h2>
-          <h2 onClick={() => handleSubHeadingClick('Cover Letter')}>Job Details</h2>
+        <div className='sub-headers'>
+          <h2
+            className={`sub-header ${selectedSubHeading === 'Chat Help' ? 'selected' : ''}`}
+            onClick={() => handleSubHeadingClick('Chat Help')}
+          >
+            Chat
+          </h2>
+          <h2
+            className={`sub-header ${selectedSubHeading === 'Cover Letter' ? 'selected' : ''}`}
+            onClick={() => handleSubHeadingClick('Cover Letter')}
+          >
+            Job Details
+          </h2>
         </div>
-        <div>
-          {content && <p>{content}</p>}
+        <div className="content-container">
+          {content}
         </div>
       </header>
     </div>
