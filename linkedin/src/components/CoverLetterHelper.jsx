767d82a0be519711801a92576e04dcb751c90a23
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-17 19:21:42 +0530

    function handleLetter, to pass the resume and job details to backedn

--- linkedin/src/components/CoverLetterHelper.jsx
+++ linkedin/src/components/CoverLetterHelper.jsx
@@ -5,13 +5,13 @@ const CoverLetterHelper = () => {
   const [result, setResult] = useState("");
   const [resume, setResume] = useState("");
   const handleFileUpload = (event) => {
-    setResume (event.target.files[0])
-  }
+    setResume(event.target.files[0]);
+  };
   useEffect(() => {
     const handleJobDetails = (event) => {
       if (event.data && event.data.type === "job-details-text") {
         const job_details = event.data.text;
-        setJobDetails(job_details)
+        setJobDetails(job_details);
       }
     };
     window.addEventListener("message", handleJobDetails);
@@ -19,24 +19,44 @@ const CoverLetterHelper = () => {
       window.removeEventListener("message", handleJobDetails);
     };
   }, []);
-
-
+  async function handleLetter () {
+    const formData = new FormData();
+    formData.append("resume",resume)
+    formData.append("job details",jobDetails)
+    try {
+      const response = await fetch("http://localhost:5000/coverLetter", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        const result = await response.text();
+        console.log(jobDetails)
+        console.log(resume);
+        setResult(result)
+      } else {
+        alert("Error: Something went wrong");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
   return (
     <div className="input-container">
       <label htmlFor="csv-upload">
-        <button variant="contained" component="span" onClick= {handleFileUpload}>
+        <button variant="contained" component="span" onClick={handleFileUpload}>
           Upload
         </button>
         <input
           type="file"
-          id="csv-upload"
-          accept=".csv"
+          id="pdf-upload"
+          accept=".pdf"
+          required={true}
           style={{ display: "none" }}
         />
         {resume ? (
           <div>
             <p>File uploaded successfully!</p>
-            <button >Generate Cover Letter</button>
+            <button onClick={handleLetter} >Generate Cover Letter</button>
           </div>
         ) : (
           <div>
