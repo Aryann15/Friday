91d5bdb744be600eb92da4c934c98f75f5bdba0d
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-17 19:15:14 +0530

    upload resume function

--- linkedin/src/components/CoverLetterHelper.jsx
+++ linkedin/src/components/CoverLetterHelper.jsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from "react";
 
 const CoverLetterHelper = () => {
-    const [jobDetails, setJobDetails] = useState ("")
-    const [result,setResult] = useState ("")
-    const [resume,setResume] = useState ("")
-  
-    return (
-          <div className="input-container">
-            <input
-              type="text"
-              placeholder="Job details"
-              required={true}
-              className="input-field"
-              onChange={handleNameChange}
-            />
-            <button onClick={handleMessages} className="submit-button">Submit</button>
+  const [jobDetails, setJobDetails] = useState("");
+  const [result, setResult] = useState("");
+  const [resume, setResume] = useState("");
+  const handleFileUpload = (event) => {
+    setResume (event.target.files[0])
+  }
+  return (
+    <div className="input-container">
+      <label htmlFor="csv-upload">
+        <button variant="contained" component="span" onClick= {handleFileUpload}>
+          Upload
+        </button>
+        <input
+          type="file"
+          id="csv-upload"
+          accept=".csv"
+          style={{ display: "none" }}
+        />
+        {resume ? (
+          <div>
+            <p>File uploaded successfully!</p>
+            <button >Generate Cover Letter</button>
           </div>
-    );
-  };
-  
-  export default App;
-  
+        ) : (
+          <div>
+            <p>Upload your resume please </p>
+          </div>
+        )}
+      </label>
+    </div>
+  );
+};
+export default CoverLetterHelper;
