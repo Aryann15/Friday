f3c5532091830b30fa88b887076eb57bc9768309
Raj Aryan Singh <rajlysm15@gmail.com>
2023-11-18 17:16:31 +0530

    structure

--- linkedin/public/popup.js
+++ linkedin/public/popup.js
@@ -6,8 +6,11 @@ chrome.runtime.onMessage.addListener((request, sender) => {
   if (request.type === "chat-text") {
     const chat = request.text;
     console.log("chat " + chat)
+    window.postMessage({ type: "chat-text", text: chat }, "*");
   }
   if (request.type === "job-details-text") {
     const job_details = request.text;
-    console.log("job details " + job_details)}
+    console.log("job details " + job_details)
+    window.postMessage({ type: "job-details-text", text: job_details }, "*");
+}
 });
